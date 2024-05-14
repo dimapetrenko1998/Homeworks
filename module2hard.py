625f03416420ec12ad323677ec4beae8c1848fff
dima <petrenkodima083@gmail.com>
2024-05-14 18:35:44 +0300

    Delete module2hard.py

--- module2hard.py
+++
@@ -1,9 +0,0 @@
-numbers = int(input('Введите число от 3 до 20:'))
-result = ''
-
-for i in range(1, numbers):
-    for j in range(i + 1, numbers + 1):
-        if numbers % (i + j) == 0:
-            if str(i) + str(j) not in result and str(j) + str(i) not in result:
-                result += str(i) + str(j)
-print(result)
