19ae2a9f171c20d63ceed4504c3b3e070e68b8fc
dima <petrenkodima083@gmail.com>
2024-05-14 18:22:45 +0300

    Add files via upload

---
+++ module 1/module1hard.py
@@ -0,0 +1,7 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+
+average_grades = {student: sum(grade) / len(grade) for student, grade in zip(students, grades)}
+sorted_students = sorted(students)
+print(average_grades)
+
