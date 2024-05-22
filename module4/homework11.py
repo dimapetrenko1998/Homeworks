204eda5f66a2dd6dbbc45674cd38330a45970dab
dima <petrenkodima083@gmail.com>
2024-05-22 21:53:42 +0300

    commit

---
+++ module4/homework11.py
@@ -0,0 +1,12 @@
+def str_():
+    print("Hello, world")
+
+
+str_()
+
+
+def string_():
+    print("Hello, world")
+
+
+string_()
---
+++ module4/main.py
@@ -0,0 +1,4 @@
+import homework11 as hw11
+
+hw11.str_()
+hw11.string_()
