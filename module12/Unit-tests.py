e6adb433fbf4476b7af4358bae1356b75347c9fc
dima <petrenkodima083@gmail.com>
2024-08-17 20:09:32 +0300

    commit

--- module12/Unit-tests.py
+++ module12/Unit-tests.py
@@ -4,26 +4,26 @@ import unittest
 
 class RunnerTest(unittest.TestCase):
     def test_walk(self):
-        runner = Runner("Тестовый бегун")  # Создаем объект класса Runner с произвольным именем
-        for _ in range(10):  # Вызываем метод walk 10 раз
+        runner = Runner("Тестовый бегун")
+        for _ in range(10):
             runner.walk()
-        self.assertEqual(runner.distance, 50)  # Проверяем, что расстояние равно 50
+        self.assertEqual(runner.distance, 50)
 
     def test_run(self):
-        runner = Runner("Тестовый бегун 2")  # Создаем объект класса Runner с произвольным именем
-        for _ in range(10):  # Вызываем метод run 10 раз
+        runner = Runner("Тестовый бегун 2")
+        for _ in range(10):
             runner.run()
-        self.assertEqual(runner.distance, 100)  # Проверяем, что расстояние равно 100
+        self.assertEqual(runner.distance, 100)
 
     def test_challenge(self):
-        runner1 = Runner("Бегун 1")  # Создаем первого бегуна
-        runner2 = Runner("Бегун 2")  # Создаем второго бегуна
+        runner1 = Runner("Бегун 1")
+        runner2 = Runner("Бегун 2")
 
-        for _ in range(10):  # Вызываем методы run и walk 10 раз
+        for _ in range(10):
             runner1.run()
             runner2.walk()
 
-        self.assertNotEqual(runner1.distance, runner2.distance)  # Проверяем, что расстояния не равны
+        self.assertNotEqual(runner1.distance, runner2.distance)
 
 
 if __name__ == '__main__':
