ae8710f3f78929f54f97f9fcc065244e076267fe
dima <petrenkodima083@gmail.com>
2024-09-20 19:28:04 +0300

    commit

--- urbanbot/urbanbot.py
+++ urbanbot/urbanbot.py
@@ -3,6 +3,7 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from api import api
 
 api = api
@@ -24,16 +25,36 @@ kb = ReplyKeyboardMarkup(
     resize_keyboard=True
 )
 
+inline_kb = InlineKeyboardMarkup(row_width=2)
+inline_kb.add(
+    InlineKeyboardButton(text="Рассчитать норму калорий", callback_data='calories'),
+    InlineKeyboardButton(text="Формулы расчёта", callback_data='formulas')
+)
+
 
 @dp.message_handler(commands=["start"])
 async def start_command(message: types.Message):
-    await message.answer("Добро пожаловать! Выберите опцию:", reply_markup=kb)
+    await message.answer("Привет! Я бот, помогающий твоему здоровью.", reply_markup=kb)
 
 
 @dp.message_handler(text="Рассчитать")
-async def set_age(message: types.Message, state: FSMContext):
-    await message.answer("Введите свой возраст:")
+async def main_menu(message: types.Message):
+    await message.answer("Выбери опцию:", reply_markup=inline_kb)
+
+
+@dp.callback_query_handler(text='formulas')
+async def get_formulas(call: types.CallbackQuery):
+    await call.message.answer(
+        "10 x вес (кг) + 6,25 x рост (см) – 5 x возраст (г) – 161"
+    )
+    await call.answer()
+
+
+@dp.callback_query_handler(text='calories')
+async def set_age(call: types.CallbackQuery, state: FSMContext):
+    await call.answer("Введите свой возраст:")
     await UserState.age.set()
+    await call.answer()
 
 
 @dp.message_handler(state=UserState.age)
