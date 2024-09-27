0ced90728871033d718c93cf8c736e9d135ae5a4
dima <petrenkodima083@gmail.com>
2024-09-27 13:18:55 +0300

    commit

--- urbanbot/urbanbot.py
+++ urbanbot/urbanbot.py
@@ -12,15 +12,24 @@ bot = Bot(token=api)
 
 dp = Dispatcher(bot, storage=MemoryStorage())
 
+# Главная клавиатура
 start_kb = ReplyKeyboardMarkup(
     keyboard=[
         [
             KeyboardButton(text='Рассчитать'),
-            KeyboardButton(text="Информация")
+            KeyboardButton(text="Информация"),
+            KeyboardButton(text="Купить")  # Добавлена кнопка "Купить"
         ]
     ], resize_keyboard=True
 )
 
+# Inline клавиатура для выбора продуктов в ряд
+inline_keyboard = InlineKeyboardMarkup(row_width=4)  # Устанавливаем ширину строки
+for i in range(1, 5):
+    button = InlineKeyboardButton(text=f"Product{i}", callback_data="product_buying")
+    inline_keyboard.add(button)
+
+# Клавиатура для расчета калорий
 next_kb = InlineKeyboardMarkup(
     inline_keyboard=[
         [InlineKeyboardButton(text='Рассчитать норму калорий', callback_data='calories')],
@@ -36,6 +45,23 @@ class UserState(StatesGroup):
     weight = State()
 
 
+@dp.message_handler(lambda message: message.text == "Купить")
+async def get_buying_list(message: types.Message):
+    for i in range(1, 5):
+        product_description = f'Название: Product{i} | Описание: описание {i} | Цена: {i * 100}'
+        await message.answer(product_description)  # Отправляем описание продукта
+        await bot.send_photo(message.chat.id,
+                             photo='https://cdn5.imgbb.ru/user/36/369623/201405/a00fc3d46d90c2dbfc5745e95bc893c3.jpg')
+
+    await message.answer("Выберите продукт для покупки:", reply_markup=inline_keyboard)
+
+
+@dp.callback_query_handler(lambda call: call.data == "product_buying")
+async def send_confirm_message(call: types.CallbackQuery):
+    await call.answer("Вы успешно приобрели продукт!")
+    await bot.send_message(call.from_user.id, "Вы успешно приобрели продукт!")
+
+
 @dp.message_handler(commands=['start'])
 async def start_message(message):
     await message.answer('Привет! Я бот помогающий твоему здоровью.', reply_markup=start_kb)
@@ -43,7 +69,7 @@ async def start_message(message):
 
 @dp.message_handler(text='Информация')
 async def inform(message):
-    await message.answer('Бот умеет считать калории, нажмите кнопку "Расcчитать')
+    await message.answer('Бот умеет считать калории, нажмите кнопку "Рассчитать"')
 
 
 @dp.message_handler(text='Рассчитать')
@@ -81,7 +107,6 @@ async def set_weight(message, state):
 
 @dp.message_handler(state=UserState.weight)
 async def send_calories(message, state):
-    global calories
     await state.update_data(weight=message.text)
     data = await state.get_data()
     age = int(data['age'])
@@ -93,8 +118,9 @@ async def send_calories(message, state):
     elif sex == 'ж':
         calories = (10 * weight) + (6.25 * growth) - (5 * age) - 161
     else:
-        await message.answer('Некоректно указан пол,пожалуйста повторите попытку!')
-    await message.answer(f'Ваша норма каллорий: {calories} ккал')
+        await message.answer('Некоректно указан пол, пожалуйста, повторите попытку!')
+        return
+    await message.answer(f'Ваша норма калорий: {calories} ккал')
     await state.finish()
 
 
@@ -103,9 +129,10 @@ async def get_formulas(call):
     await call.message.answer('''
     Рассчет калорий производится по формуле Миффлина-Сан Жеора
 
-    Для женщин :10 x вес (кг) + 6,25 x рост (см) – 5 x возраст (г) – 161
+    Для женщин: 10 x вес (кг) + 6,25 x рост (см) – 5 x возраст (г) – 161
 
-    Для мужчин :10 х вес (кг) + 6,25 x рост (см) – 5 х возраст (г) + 5''')
+    Для мужчин: 10 x вес (кг) + 6,25 x рост (см) – 5 x возраст (г) + 5
+    ''')
 
 
 if __name__ == "__main__":
