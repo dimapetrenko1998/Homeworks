266204375a03dae791e1ef4e6cae687fb8a08438
dima <petrenkodima083@gmail.com>
2024-10-24 18:12:19 +0300

    commit

--- module16/Pydantic Data Models.py
+++ module16/Pydantic Data Models.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI, HTTPException, Request
-from starlette.templating import Jinja2Templates
-from starlette.responses import HTMLResponse
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 from pydantic import BaseModel
+from typing import List
 
 app = FastAPI()
 
@@ -9,40 +10,69 @@ app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
 
-# Модель пользователя
+users = []
+
+
+
 class User(BaseModel):
     id: int
     username: str
     age: int
 
 
-users = []
 
-
-@app.get("/")
-async def read_root(request: Request) -> HTMLResponse:
+@app.get("/", response_class=HTMLResponse)
+def read_root(request: Request):
     return templates.TemplateResponse("users.html", {"request": request, "users": users})
 
 
-@app.get("/users/{user_id}")
-async def get_user(request: Request, user_id: int) -> HTMLResponse:
-    user = next((user for user in users if user.id == user_id), None)
-    if user is None:
-        raise HTTPException(status_code=404, detail="User not found")
-    return templates.TemplateResponse("users.html", {"request": request, "user": user})
+@app.get("/users", response_model=List[User])
+def get_users():
+    return users
+
+
+
+@app.get("/users/{user_id}", response_class=HTMLResponse)
+def get_user(request: Request, user_id: int):
+    for user in users:
+        if user.id == user_id:
+            return templates.TemplateResponse("users.html", {"request": request, "user": user})
+    raise HTTPException(status_code=404, detail="User was not found")
+
 
 
 @app.post("/user/{username}/{age}", response_model=User)
-async def create_user(username: str, age: int) -> User:
-    user_id = len(users) + 1  # Генерация нового ID
+def create_user(username: str, age: int):
+    user_id = (users[-1].id + 1) if users else 1
     new_user = User(id=user_id, username=username, age=age)
     users.append(new_user)
     return new_user
 
 
-# Создаем несколько пользователей
+
+@app.put("/user/{user_id}/{username}/{age}", response_model=User)
+def update_user(user_id: int, username: str, age: int):
+    for user in users:
+        if user.id == user_id:
+            user.username = username
+            user.age = age
+            return user
+    raise HTTPException(status_code=404, detail="User was not found")
+
+
+
+@app.delete("/user/{user_id}", response_model=User)
+def delete_user(user_id: int):
+    for user in users:
+        if user.id == user_id:
+            users.remove(user)
+            return user
+    raise HTTPException(status_code=404, detail="User was not found")
+
+
+
 @app.on_event("startup")
-async def startup_event():
-    create_user("UrbanUser", 24),
-    create_user("UrbanTest", 22),
-    create_user("Capybara", 60),
+def startup_event():
+    create_user("UrbanUser", 24)
+    create_user("UrbanTest", 22)
+    create_user("Capybara", 60)
